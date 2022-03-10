a1c787cbaaf56ba615ddd2a67d4dd162ab44380f
barisarabasuren <63669033+barisarabasuren@users.noreply.github.com>
2022-03-10 21:07:01 +0300

    cors

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors')
 const carsRouter = require('./routes/cars.router')
 
-
 const app = express();
 
 app.use(cors({
-    origin: 'https://car-listing-umut.netlify.app/'
+    origin: [
+        'https://car-listing-umut.netlify.app',
+        'http://localhost:3000'
+    ]
 }))
 
 app.use(express.json());
